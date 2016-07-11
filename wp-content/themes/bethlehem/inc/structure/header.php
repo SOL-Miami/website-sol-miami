5515cd30045d9167c56a3e3732a83e0f813c3a0b
Mark Natera <marknatera@gmail.com>
2016-07-11 19:52:59 -0400

    Added livestream button

--- wp-content/themes/bethlehem/inc/structure/header.php
+++ wp-content/themes/bethlehem/inc/structure/header.php
@@ -237,6 +237,7 @@ if ( ! function_exists( 'bethlehem_navigation_links' ) ) {
 		<div class="top-nav-links">
 			<?php if( is_array( $btn_args ) && ! empty( $btn_args ) ) : ?>
 				<ul>
+					<li class="live-stream"><img src="/wp-content/uploads/2016/07/livestream-logo-inverted.png" alt=""/></li>
 					<?php foreach ( $btn_args as $btn ) : ?>
 						<li>
 							<?php
